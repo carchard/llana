8f755ad4e0217f67ececb639b4cf2b32294496ce
ConnorArchard <cwa37@cornell.edu>
2016-12-15 22:19:36 -0500

    starting off

---
+++ sandbox.py
@@ -0,0 +1,3 @@
+# place for testing out packages before integration into llana
+import pyaudio
+
---
+++ src/llana_main.py
@@ -0,0 +1,13 @@
+# authors:
+#  connor archard
+#  geoff keating
+#
+# updated:
+#  12-15-2016
+#
+# brief:
+#  main file for llana operation
+
+
+if __name__ == "__main__":
+    print "Hello world"
